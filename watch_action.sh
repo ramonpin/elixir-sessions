c6bbdd3c9c133ed8cc53b39efb2895cd5b24b8d1
ramonpin <ramon.pin@gmail.com>
2021-09-15 19:17:31 +0200

    feat: Initial version

---
+++ watch_action.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env zsh
+
+# Launch mix to run tests
+clear
+unbuffer mix test 2>&1 | tee /dev/shm/watch_action.txt
+
+if [[ ${pipestatus[1]} -eq 0 ]]; then
+  ICON=info
+else
+  ICON=error
+fi
+
+# Grab last three lines and generate a notification
+LAST_LINES=$(tail -4 /dev/shm/watch_action.txt | sed -e "s/\x1b\[.\{1,5\}m//g")
+notify-send -i $ICON -t 10000 "mix test" "$LAST_LINES"
+
