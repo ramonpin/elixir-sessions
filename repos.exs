2facefffacc747a4fc783bd701788adbda1cc6f4
ramonpin <ramon.pin@gmail.com>
2021-09-17 12:04:45 +0200

    feat: Sample script with Mix.install

---
+++ repos.exs
@@ -0,0 +1,26 @@
+# github-repos.exs
+Mix.install([
+  {:jason, ">= 1.0.0"},
+  {:httpoison, "~> 1.8"}
+])
+
+defmodule GithubRepos do
+  def load(username) do
+    "https://api.github.com/users/#{username}/repos"
+    |> HTTPoison.get!()
+    |> Map.get(:body)
+    |> Jason.decode!()
+    |> Enum.reject(fn %{"fork" => fork} -> fork end)
+    |> do_some_calc()
+    |> Jason.encode!()
+  end
+
+  defp do_some_calc(data) do 
+    data
+    |> Enum.map(&Map.take(&1, ~w[full_name fork pushed_at]))
+  end
+end
+
+"ramonpin"
+|> GithubRepos.load()
+|> IO.puts()
